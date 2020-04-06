f33dfb761ad1c0abfbc6219d15d0ad0307cd5b3f
gabriel2302 <gabriel23002@gmail.com>
2020-04-06 17:10:57 -0300

    Adicionados os campos instagram e facebook na api

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -15,7 +15,9 @@ routes.post('/ongs', celebrate({
         email: Joi.string().required().email(),
         whatsapp: Joi.string().required().min(10).max(11),
         city: Joi.string().required(),
-        uf: Joi.string().required().length(2)
+        uf: Joi.string().required().length(2),
+        facebook: Joi.string().required(),
+        instagram: Joi.string().required(),
     })
 }), OngController.create);
 
